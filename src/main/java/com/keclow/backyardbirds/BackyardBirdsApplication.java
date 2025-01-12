637772860678989dcdf69068b905ea954a2db401
Katrina <contact@katrinaclow.com>
2025-01-12 14:37:59 -0400

    Initial project setup with basic package structure

---
+++ src/main/java/com/keclow/backyardbirds/BackyardBirdsApplication.java
@@ -0,0 +1,13 @@
+package com.keclow.backyardbirds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackyardBirdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackyardBirdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keclow/backyardbirds/BackyardBirdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keclow.backyardbirds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackyardBirdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
