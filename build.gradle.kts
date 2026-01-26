a7f62990a13b6a599a3aa229e7d10ed32ea2e47c
katrinaclow <contact@katrinaclow.com>
2026-01-26 15:15:56 -0400

    update gradle settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,12 @@ dependencies {
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-config-yaml")
     implementation("io.ktor:ktor-client-cio:3.4.0")
+    implementation("io.ktor:ktor-client-serialization:3.4.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.ktor:ktor-client-mock:3.4.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
 }
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,10 +1,14 @@
 package ca.backyardbirds
 
+import io.github.cdimascio.dotenv.dotenv
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+
+val dotenv = dotenv { ignoreIfMissing = true }
+val ebirdApiKey = dotenv["EBIRD_API_KEY"] ?: System.getenv("EBIRD_API_KEY")
 
 fun main() {
     embeddedServer(Netty, port = 8080, module = Application::module).start(wait = true)
